b71e35d3201a676e79833ce25e70a25312aa7fcd
Minh Ngo Tuan <ngotuanminh98@gmail.com>
2023-04-24 01:28:24 +0700

    update

--- src/components/ChooseCurrency.js
+++ src/components/ChooseCurrency.js
@@ -1,7 +1,7 @@
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import AdbIcon from '@mui/icons-material/Adb';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Grid } from '@mui/material';
+import { Grid, Modal } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
@@ -12,17 +12,31 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import React, { useState } from 'react';
 import localStorageHelper from '../utils/localStorage';
 import { useNavigate, useParams } from 'react-router';
 
 const pages = ['HOME', 'NFT VERIFICATION', 'PRESS RELEASe'];
 // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 function ChooseCurrency() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
   // const [anchorElUser, setAnchorElUser] = React.useState(null);
   const addressMetaMask = localStorageHelper.get('addressMetaMask');
+
+  const [isOpenModalConfirm, setIsOpenModalConfirm] = useState(false);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -32,6 +46,14 @@ function ChooseCurrency() {
     setAnchorElNav(null);
   };
 
+  const handleCloseModalConfirm = () => {
+    setIsOpenModalConfirm(false);
+  }
+
+  const handleOpenModalConfirm = () => {
+    setIsOpenModalConfirm(true);
+  }
+
   const { tokenHash } = useParams();
   const navigate = useNavigate();
 
@@ -40,19 +62,19 @@ function ChooseCurrency() {
       id: 1,
       name: "Ethereum",
       icon: "https://www.spectre.ai/assets/images/assets/ETH-logo.png?v=2.13",
-      onClick: () => stakeNFT(tokenHash)
+      onClick: handleOpenModalConfirm
     },
     {
       id: 2,
       name: "Binance Smart Chain",
       icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJck7-kfeVY1jE5HKfhZTjogzepWqnk28MkIy6UR-_AqL5axCHHiUlCVNYrhXy2h0CJY8&usqp=CAU",
-      onClick: () => stakeNFT(tokenHash)
+      onClick: handleOpenModalConfirm
     },
     {
       id: 3,
       name: "Solana",
       icon: "https://s2.coinmarketcap.com/static/img/coins/200x200/5426.png",
-      onClick: () => stakeNFT(tokenHash)
+      onClick: handleOpenModalConfirm
     },
   ];
 
@@ -71,6 +93,32 @@ function ChooseCurrency() {
       localStorageHelper.setObject('tokenStaked', newTokenStaked);
     }
     navigate(`/my-nft/${addressMetaMask}`);
+  }
+
+
+  const ModalConfirm = () => {
+    return (
+      <Modal
+        open={isOpenModalConfirm}
+        onClose={handleCloseModalConfirm}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" textAlign='center' variant="h6" component="h2" style={{marginBottom: 20}}>
+            Do you want to stake this NFT?
+          </Typography>
+
+          <div style={{display: 'flex', height: 60}}>
+          <Button className="buttonDisabled" variant="Continue" sx={{borderRadius:10, textTransform: 'capitalize', color:"black", background:'#4DFFF4',width:170, fontWeight: '700' , height: 50,':hover': {bgcolor: '#4DFFF4',}, }} onClick={() => stakeNFT(tokenHash)}>Continue</Button>
+          <Typography sx={{fontSize:14, cursor:'pointer', marginLeft: 12, display: 'flex', justifyContent: 'center', alignItems: 'center'}} onClick={handleCloseModalConfirm}>
+            Cancel
+          </Typography>
+          </div>
+
+        </Box>
+      </Modal>
+    )
   }
 
   return (
@@ -184,7 +232,7 @@ function ChooseCurrency() {
         </Grid>  
         <Grid item xs={3} sx={{ pt:5 }}>
         {Icons.map(list=>(
-            <div key={list.id} style={{color:'white',border:'1px solid white', width:'300px', padding:18, marginBottom:20 , height:20,borderRadius:30 ,cursor:'pointer'}} onClick={list?.onClick}>
+            <div key={list.id} style={{color:'white',border:'1px solid white', width:'300px', padding:18, marginBottom:20 , height:20,borderRadius:30 ,cursor:'pointer'}} onClick={list.onClick}>
               <div style={{display:'inline-flex', float:'left'}}>
                 <img alt='' style={{width:'20px', height:'20px', marginRight:10}} src={list.icon}></img> {list.name}
               </div>
@@ -193,11 +241,13 @@ function ChooseCurrency() {
         </Grid>  
         <Grid item xs={3} sx={{ pt:5 }}>
         <Button className="buttonDisabled" variant="Continue" sx={{borderRadius:10, textTransform: 'capitalize', color:"black", background:'#4DFFF4',width:170, fontWeight: '700' , ':hover': {bgcolor: '#4DFFF4',}, }}>Continue</Button>
-          <Typography sx={{color:'white', fontSize:14,pt:3, pb:20 , cursor:'pointer'}} onClick={() => navigate(`/my-nft/${addressMetaMask}`)}>
-            Cancel
-          </Typography>
+        <Typography sx={{color:'white', fontSize:14,pt:3, pb:20 , cursor:'pointer'}} onClick={() => navigate(`/my-nft/${addressMetaMask}`)}>
+          Cancel
+        </Typography>
         </Grid>  
       </Grid> 
+
+      <ModalConfirm />
       
     </div>
     
